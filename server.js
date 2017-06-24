f08a259e7d863e5e526f746cc3fcf4a71ad661d7
bryan.modula <bryan.modula@gmail.com>
2017-06-24 11:15:20 -0400

    server.js updated

--- server.js
+++ server.js
@@ -16,6 +16,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+
+//create two variables for tables and reservations
+
+var tables = [{
+	name: "Origin Master",
+	phoneNumer: "732-234-5678",
+	email: "origin@aol.com",
+	uniqueId: "73111"
+}]
+
+var reservations = [{
+	name: "Davey D",
+	phoneNumer: "732-342-9876",
+	email: "davey@aol.com",
+	uniqueId: "72211"
 // Routes and logs server is listening
 
 app.get("/", function(req, res) {
@@ -23,6 +38,10 @@ app.get("/", function(req, res) {
 });
 
 // GET reserve - reserve.html
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "add.html"));
+});
 
 // GET tables reservations.html
 
